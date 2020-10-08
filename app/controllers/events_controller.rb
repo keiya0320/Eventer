c0c6dc5165d4c62022756467eeb59ef6675b0da8
keiya0320 <keiya.man@icloud.com>
2020-10-08 12:46:02 +0900

    詳細機能、削除機能の実装

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -11,10 +11,19 @@ class EventsController < ApplicationController
     Event.create(event_params)
   end
 
+  def destroy
+    event = Event.find(params[:id])
+    event.destroy
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
   private
 
   def event_params
-    params.require(:event).permit(:title, :explanation, :done_at, :people, :address).merge(user_id: current_user.id)
+    params.require(:event).permit(:title, :image, :explanation, :done_at, :people, :address).merge(user_id: current_user.id)
   end
 
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -4,8 +4,13 @@ class Event < ApplicationRecord
   belongs_to :user
 
   validates :title, presence: true
-  validates :explanation, presence: true
+  validates :explanation, presence: true, unless: :was_attached?
   validates :done_at, presence: true
   validates :people, presence: true
   validates :address, presence: true
+
+
+  def was_attached?
+    self.image.attached?
+  end
 end
